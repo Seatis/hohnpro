c5dac444607cde009669c5470ad598d3942a4ccd
Attila Kezer <kezer.attila@mikrum.hu>
2020-07-07 15:28:03 +0200

    uj hir

---
+++ src/db/insert_news_20200707.sql
@@ -0,0 +1,23 @@
+SET NAMES utf8;
+SET character_set_client = utf8;
+
+INSERT INTO hohnhu_angular.hohn_news(cim, datum, htmlContent, kategoria, kepMainUrl, kepSecUrl, szerzo, videoUrl, rovidLeiras) VALUES (
+    'Valóban van probléma és azt kezelni kell az iskolákban',
+    '2020-07-03T10:00:00',
+    '<p>Hohn Krisztina parlamenti felszólalása - 2020. július 3.</p>',
+    'parlament',
+    'assets/images/news/general/general_2.jpg',
+    'assets/images/news/general/general_sec_1.jpg',
+    'Hohn Krisztina',
+    'https://www.youtube.com/embed/FWMdfnP4JOs',
+    'Hohn Krisztina parlamenti felszólalása - 2020. július 3.');
+INSERT INTO hohnhu_angular.hohn_news(cim, datum, htmlContent, kategoria, kepMainUrl, kepSecUrl, szerzo, videoUrl, rovidLeiras) VALUES (
+    '10 év eltelt, miért nem adták vissza a 13. havi nyugdíjat?',
+    '2020-07-03T14:00:00',
+    '<p>Hohn Krisztina parlamenti felszólalása - 2020. július 3.</p>',
+    'parlament',
+    'assets/images/news/general/general_2.jpg',
+    'assets/images/news/general/general_sec_1.jpg',
+    'Hohn Krisztina',
+    'https://www.youtube.com/embed/XpJ3VrYzp50',
+    'Hohn Krisztina parlamenti felszólalása - 2020. július 3.');
