5a7f169ac78e5e721ac2a1df3e4d0be130694b3b
Attila Kezer <kezer.attila@mikrum.hu>
2020-08-06 15:03:28 +0200

    lightwidget https change

--- src/app/common/hohn.util.ts
+++ src/app/common/hohn.util.ts
@@ -1,6 +1,7 @@
 export class HohnUtil {
 
-  static readonly instaUrl: string = '//lightwidget.com/widgets/5a8455a2e8f4560ca25a8f721b3a19db.html';
+  //static readonly instaUrl: string = '//lightwidget.com/widgets/5a8455a2e8f4560ca25a8f721b3a19db.html';
+  static readonly instaUrl: string = 'https://cdn.lightwidget.com/widgets/5a8455a2e8f4560ca25a8f721b3a19db.html';
   static readonly instaHomeUrl: string = 'https://www.instagram.com/hohnkriszta/';
   static readonly facebookUrl: string = 'https://www.facebook.com/hohnkrisztina/';
   static readonly youTubeChannelUrl: string = 'https://www.youtube.com/channel/UCyG0Db9NOIunagFAu6qlCLQ';
