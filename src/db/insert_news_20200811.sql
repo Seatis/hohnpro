2e814ade27e554754dc31d85ad2fad3b35e4a31c
Attila Kezer <kezer.attila@mikrum.hu>
2020-08-11 23:07:46 +0200

    tsz szerzodes implementalasa

---
+++ src/db/insert_news_20200811.sql
@@ -0,0 +1,33 @@
+SET NAMES utf8;
+SET character_set_client = utf8;
+
+INSERT INTO hohnhu_angular.hohn_news(cim, datum, htmlContent, kategoria, kepMainUrl, kepSecUrl, szerzo, videoUrl, rovidLeiras) VALUES (
+    'Társadalmi szerződés',
+    '2020-08-11T10:00:00',
+    '<div class="align-center">
+    <img src="assets/images/site/uk_tsz_logo.jpg" alt="Új Kezdet">
+</div>
+<p><strong>Tisztelt Honfit&aacute;rsaink!</strong></p>
+<p><strong>Tisztelt V&aacute;laszt&oacute;polg&aacute;rok!</strong></p>
+<p>&nbsp;</p>
+<div style="text-align: center;"><strong>NYILV&Aacute;NOSS&Aacute;GRA HOZZUK AZ &Uacute;J T&Aacute;RSADALMI SZERZŐD&Eacute;S ELSŐ TERVEZET&Eacute;T</strong></div></br>
+<a href="assets/pdf/tarsadalmi_szerzodes.pdf">A társadalmi szerződés letöltéséhez kattintson ide!</a>
+<p>Mi is m&eacute;lyre hat&oacute; v&aacute;ltoz&aacute;st akarunk. B&eacute;k&eacute;s, &eacute;p&iacute;tő k&ouml;z&eacute;letet, a tehets&eacute;gek megbecs&uuml;l&eacute;s&eacute;t, valamint azt, hogy a k&ouml;zp&eacute;nzen fenntartott m&eacute;dia l&eacute;pjen fel a hamis h&iacute;rek ellen.</p>
+<p>Azt akarjuk, hogy haz&aacute;nk erőforr&aacute;sait a megfelelő eg&eacute;szs&eacute;g&uuml;gyre, sz&iacute;nvonalas oktat&aacute;sra, szabad tudom&aacute;nyra, fontos infrastrukt&uacute;r&aacute;kra, az innovat&iacute;v hazai v&aacute;llalkoz&aacute;sok kiemelt t&aacute;mogat&aacute;s&aacute;ra, k&ouml;rnyezet &eacute;s term&eacute;szetv&eacute;delemre, &eacute;lelmiszer &ouml;nrendelkez&eacute;sre, erős &ouml;nkorm&aacute;nyzatis&aacute;gra ford&iacute;thassuk.</p>
+<p>A puszt&iacute;t&oacute; k&ouml;zpontos&iacute;t&aacute;s &eacute;s a mesters&eacute;ges, hűb&eacute;ri elitgy&aacute;rt&aacute;s helyett a tehets&eacute;gek verseny&eacute;ben b&iacute;zunk.</p>
+<p>B&iacute;zunk abban, hogy nem maradhat el az elsz&aacute;moltat&aacute;s, az anyagi &eacute;s az erk&ouml;lcsi felelőss&eacute;gre von&aacute;s.</p>
+<p>Be kell fejezn&uuml;nk az elcsalt rendszerv&aacute;lt&aacute;st. Nyitott&aacute; tenni t&ouml;rt&eacute;nelm&uuml;nk s&ouml;t&eacute;t akt&aacute;it &eacute;s k&iacute;m&eacute;letlen&uuml;l szembe n&eacute;zni a t&eacute;nyekkel.</p>
+<p>V&aacute;ltoz&aacute;s sz&uuml;ks&eacute;ges, mi hozz&aacute; akarunk j&aacute;rulni a v&aacute;ltoz&aacute;shoz azzal is, hogy kellő hittel &eacute;s al&aacute;zattal, sok tapasztalattal &eacute;s tud&aacute;ssal, gyakorlati l&aacute;t&aacute;sm&oacute;ddal elk&eacute;sz&iacute;tett&uuml;k az &Uacute;J T&Aacute;RSADALMI SZERZŐD&Eacute;S első tervezet&eacute;t.</p>
+<p>K&ouml;z&ouml;s kinccs&eacute;, kiindul&aacute;si pontt&aacute; tessz&uuml;k ezt a demokratikus ellenz&eacute;ki oldalon.</p>
+<p><strong>Munkaanyagunk, h&oacute;napok &oacute;ta a hazai ellenz&eacute;k vezetőinek asztal&aacute;n van!</strong></p>
+<p>A Magyar t&aacute;rsadalom a durva megoszt&aacute;s, k&ouml;zp&eacute;nzen finansz&iacute;rozott gyűl&ouml;letkelt&eacute;s, megannyi l&aacute;that&oacute; aljass&aacute;g miatt m&aacute;r bizalmatlan.</p>
+<p>Garanci&aacute;kra van sz&uuml;ks&eacute;g, a nagy sz&oacute;lamok &eacute;s klasszikus politikai &iacute;g&eacute;retek, elj&aacute;r&aacute;sm&oacute;dok helyett egy SZERZŐD&Eacute;SRE! Sz&aacute;mon k&eacute;rhetőre! Elk&ouml;telezetts&eacute;get jelentőre! Amely igazod&aacute;si pont, melyhez k&eacute;pest minden, nyitott szemmel j&aacute;r&oacute;, &eacute;rtő magyar ember sz&aacute;m&aacute;ra l&aacute;tszanak az elmozdul&aacute;sok, a tettek.</p>
+<p>&nbsp;</p>
+<p><strong>&Uacute;j Kezdet P&aacute;rt eln&ouml;ks&eacute;ge</strong></p>
+<p><strong>www.ujkezdet.hu</strong></p>',
+    'hir',
+    'assets/images/news/20200811/20200811_tsz.jpg',
+    'assets/images/news/general/20200811_tsz_sec.jpg',
+    'Hohn Krisztina',
+    null,
+    'NYILVÁNOSSÁGRA HOZZUK AZ ÚJ TÁRSADALMI SZERZŐDÉS ELSŐ TERVEZETÉT');
