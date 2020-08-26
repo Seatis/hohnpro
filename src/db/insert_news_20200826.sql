4dce8db703c8eb69677509c72caadeaad81246c3
Attila Kezer <kezer.attila@mikrum.hu>
2020-08-26 13:26:59 +0200

    uj hir

---
+++ src/db/insert_news_20200826.sql
@@ -0,0 +1,15 @@
+SET NAMES utf8;
+SET character_set_client = utf8;
+
+INSERT INTO hohnhu_angular.hohn_news(cim, datum, htmlContent, kategoria, kepMainUrl, kepSecUrl, szerzo, videoUrl, rovidLeiras) VALUES (
+    'El a kezekkel az önkormányzatoktól!',
+    '2020-08-26T10:00:00',
+    '<p>A korm&aacute;ny &eacute;vek &oacute;ta&nbsp; k&uuml;l&ouml;nf&eacute;le forr&aacute;smegvon&aacute;sokkal &eacute;s&nbsp; adminisztrat&iacute;v eszk&ouml;z&ouml;kkel t&ouml;rekszik arra ,hogy ellehetetlen&iacute;tse&nbsp; a ma sajt&oacute;t&aacute;j&eacute;koztat&oacute;mon elmondtam, hogy a kabinet t&ouml;rekv&eacute;s&eacute;nek eredm&eacute;nyek&eacute;nt sz&eacute;p lassan le&eacute;p&uuml;l a demokr&aacute;cia egyik alappill&eacute;r&eacute;nek sz&aacute;m&iacute;t&oacute; &ouml;nkorm&aacute;nyzati rendszer.</p>
+    <p>Komoly probl&eacute;ma, hogy rendk&iacute;v&uuml;l nehezen tal&aacute;lnak szakk&eacute;pzett, j&oacute; munkaerőt, mivel a korm&aacute;ny &eacute;vek &oacute;ta nem emeli a k&ouml;ztisztviselői alapilletm&eacute;nyt, &eacute;s nem hajland&oacute; rendezni a k&ouml;zalkalmazotti b&eacute;rt&aacute;bl&aacute;t, helyette ink&aacute;bb ki&uuml;r&iacute;ti a szektort.</p>
+    <p>Ez&eacute;rt tov&aacute;bbra is t&ouml;rv&eacute;nym&oacute;dos&iacute;t&oacute; javaslatokkal pr&oacute;b&aacute;ljuk megakad&aacute;lyozni a telep&uuml;l&eacute;sek t&ouml;nkret&eacute;tel&eacute;t. "El a kezekkel az &ouml;nkorm&aacute;nyzatokt&oacute;l!"</p>',
+    'sajtaj',
+    'assets/images/news/20200826/20200826.jpg',
+    'assets/images/news/20200826/20200826_sec.jpg',
+    'Hohn Krisztina',
+    null,
+    'Törvénymódosító javaslatokkal próbáljuk megakadályozni a települések tönkretételét.');
