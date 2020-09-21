4230491149db5e48429143766ede631d4834366a
Attila Kezer <kezer.attila@mikrum.hu>
2020-09-21 13:26:21 +0200

    uj hir

---
+++ src/db/insert_news_20200921.sql
@@ -0,0 +1,18 @@
+SET NAMES utf8;
+SET character_set_client = utf8;
+
+INSERT INTO hohnhu_angular.hohn_news(cim, datum, htmlContent, kategoria, kepMainUrl, kepSecUrl, szerzo, videoUrl, rovidLeiras) VALUES (
+    'Fogadóóra Gödöllő',
+    '2020-09-21T10:00:00',
+    '<div>2020. szeptember 29-&eacute;n 14 &oacute;r&aacute;t&oacute;l 17 &oacute;r&aacute;ig Hohn Krisztina, orsz&aacute;ggyűl&eacute;si k&eacute;pviselő fogad&oacute;&oacute;r&aacute;t tart g&ouml;d&ouml;llői irod&aacute;j&aacute;ban.</div>
+    <div>Helysz&iacute;n: 2100 G&ouml;d&ouml;llő, D&oacute;zsa Gy&ouml;rgy &uacute;t 1-3. f&ouml;ldszinti irod&aacute;ja.</div>
+    <div>&nbsp;</div>
+    <div>A v&iacute;rushelyzetre val&oacute; tekintettel előzetes bejelentkez&eacute;s sz&uuml;ks&eacute;ges:</div>
+    <div><a href="mailto:asszisztenshohn@gmail.com" target="_blank" rel="noopener">asszisztenshohn@gmail.com</a></div>
+    <div>T.: 06 30 899 3505</div>',
+    'hir',
+    'assets/images/news/20200921/20200921.jpg',
+    'assets/images/news/20200921/20200921_sec.jpg',
+    'Hohn Krisztina',
+    'https://www.youtube.com/embed/mp7nyaSrfDA',
+    '2020. szeptember 29-én 14 órától 17 óráig Hohn Krisztina, országgyűlési képviselő fogadóórát tart gödöllői irodájában.');
