e0ca7ded1dbab259ad041c4769290b14a2053586
Attila Kezer <kezer.attila@mikrum.hu>
2020-10-14 14:43:01 +0200

    uj hirek

---
+++ src/db/insert_news_20201009.sql
@@ -0,0 +1,33 @@
+SET NAMES utf8;
+SET character_set_client = utf8;
+
+INSERT INTO hohnhu_angular.hohn_news(cim, datum, htmlContent, kategoria, kepMainUrl, kepSecUrl, szerzo, videoUrl, rovidLeiras) VALUES (
+    'Törvényjavaslati vita az egészségügyi szolgálati jogviszonyról',
+    '2020-10-08T10:00:00',
+    '<p>Hohn Krisztina (Új Kezdet elnök) felszólalása az egészségügyi szolgálati jogviszonyról.</p>',
+    'parlament',
+    'assets/images/news/general/general_1.jpg',
+    'assets/images/news/general/general_sec_1.jpg',
+    'Hohn Krisztina',
+    'https://www.youtube.com/embed/iHXMnBzPSog',
+    'Hohn Krisztina (Új Kezdet elnök) felszólalása az egészségügyi szolgálati jogviszonyról.');
+INSERT INTO hohnhu_angular.hohn_news(cim, datum, htmlContent, kategoria, kepMainUrl, kepSecUrl, szerzo, videoUrl, rovidLeiras) VALUES (
+    'Könnyítsék meg a szülők dolgát koronavírusos időben',
+    '2020-10-08T14:00:00',
+    '<p>Hohn Krisztina (Új Kezdet elnök) felszólalása a szülők helyzetéről.</p>',
+    'parlament',
+    'assets/images/news/general/general_2.jpg',
+    'assets/images/news/general/general_sec_2.jpg',
+    'Hohn Krisztina',
+    'https://www.youtube.com/embed/_SJqyAaUd_A',
+    'Hohn Krisztina (Új Kezdet elnök) felszólalása a szülők helyzetéről.');
+INSERT INTO hohnhu_angular.hohn_news(cim, datum, htmlContent, kategoria, kepMainUrl, kepSecUrl, szerzo, videoUrl, rovidLeiras) VALUES (
+    'Nem elég a törvényeket szigorítani, az áldozatokat segítő ellátó rendszert is fejleszteni kell!',
+    '2020-10-18T18:00:00',
+    '<p>Hohn Krisztina (Új Kezdet elnök) felszólalása az az áldozatokat segítő ellátó rendszerrel kapcsolatban.</p>',
+    'parlament',
+    'assets/images/news/general/general_3.jpg',
+    'assets/images/news/general/general_sec_3.jpg',
+    'Hohn Krisztina',
+    'https://www.youtube.com/embed/BFYvJM1ypBk',
+    'Hohn Krisztina (Új Kezdet elnök) felszólalása az az áldozatokat segítő ellátó rendszerrel kapcsolatban.');
---
+++ src/db/insert_news_20201014.sql
@@ -0,0 +1,13 @@
+SET NAMES utf8;
+SET character_set_client = utf8;
+
+INSERT INTO hohnhu_angular.hohn_news(cim, datum, htmlContent, kategoria, kepMainUrl, kepSecUrl, szerzo, videoUrl, rovidLeiras) VALUES (
+    'Miért nem tekinti családnak a kormány az egyszülős, illetve az élettársi kapcsolatban élőket?!',
+    '2020-10-14T10:00:00',
+    '<p>Hohn Krisztina sajtótájékoztatója. - 2020. 10. 14.</p>',
+    'sajtaj',
+    'assets/images/news/general/general_sajtaj.jpg',
+    'assets/images/news/general/general_sajtaj_sec.jpg',
+    'Hohn Krisztina',
+    'https://www.youtube.com/embed/4PGWeMra6VU',
+    'Sajtótájékoztató 2020. 10. 14. - Miért nem tekinti családnak a kormány az egyszülős, illetve az élettársi kapcsolatban élőket?!');
