b1d7690e900ff8e11f6be4d837eff07338809022
Attila Kezer <kezer.attila@mikrum.hu>
2020-11-05 18:16:01 +0100

    uj hirek

---
+++ src/db/insert_news_20201105.sql
@@ -0,0 +1,25 @@
+SET NAMES utf8;
+SET character_set_client = utf8;
+
+USE hohnhu_angular;
+
+INSERT INTO hohnhu_angular.hohn_news(cim, datum, htmlContent, kategoria, kepMainUrl, kepSecUrl, szerzo, videoUrl, rovidLeiras) VALUES (
+    'Azonnali tájékoztatást a háziorvosi praxisokkal kapcsolatban!',
+    '2020-10-28T10:00:00',
+    '<p>Hohn Krisztina sajtótájékoztatója. - 2020. október 27.</p>',
+    'sajtaj',
+    'assets/images/news/general/general_sajtaj.jpg',
+    'assets/images/news/general/general_sajtaj_sec.jpg',
+    'Hohn Krisztina',
+    'https://www.youtube.com/embed/1Omldvk799M',
+    'Hohn Krisztina sajtótájékoztatója. - 2020. 10. 27.');
+INSERT INTO hohnhu_angular.hohn_news(cim, datum, htmlContent, kategoria, kepMainUrl, kepSecUrl, szerzo, videoUrl, rovidLeiras) VALUES (
+    'Kötelező lesz praxisközösséget vállalniuk a háziorvosoknak?',
+    '2020-10-29T10:00:00',
+    '<p>Hohn Krisztina parlamenti felszólalása - 2020. október 26.</p>',
+    'parlament',
+    'assets/images/news/general/general_1.jpg',
+    'assets/images/news/general/general_sec_1.jpg',
+    'Hohn Krisztina',
+    'https://www.youtube.com/embed/skwdJrN21e0',
+    'Hohn Krisztina parlamenti felszólalása - 2020. október 26.');
