ead1c05553675f1f076e3b9742334c5955dad202
Attila Kezer <kezer.attila@mikrum.hu>
2020-11-11 10:49:43 +0100

    uj hirú

---
+++ src/db/insert_news_20201110.sql
@@ -0,0 +1,15 @@
+SET NAMES utf8;
+SET character_set_client = utf8;
+
+USE hohnhu_angular;
+
+INSERT INTO hohnhu_angular.hohn_news(cim, datum, htmlContent, kategoria, kepMainUrl, kepSecUrl, szerzo, videoUrl, rovidLeiras) VALUES (
+    'Egy majdnem megtartott Népjóléti Bizottsági ülés margójára...',
+    '2020-11-10T10:00:00',
+    '<p>Egy majdnem megtartott Népjóléti Bizottsági ülés margójára...</p>',
+    'hir',
+    'assets/images/news/general/general_sajtaj.jpg',
+    'assets/images/news/general/general_sajtaj_sec.jpg',
+    'Hohn Krisztina',
+    'https://www.youtube.com/embed/7xCoGCVAhL8',
+    'Egy majdnem megtartott Népjóléti Bizottsági ülés margójára...');
