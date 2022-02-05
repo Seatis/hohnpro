01353c87b2e13f62435f500d4a724b6d57c00546
Attila Kezer <kezer.attila@mikrum.hu>
2022-02-05 18:44:35 +0100

    event backup

---
+++ src/app/common/calendar/event.backup.ts
@@ -0,0 +1,374 @@
+import {Event} from './model/event.model';
+
+export const EventBackup: Event[] = [
+  // {
+  //   id: 1,
+  //   datum: new Date('2021-07-29T22:00:00'),
+  //   cim: 'Nyilvános vita a levegőtisztaság ügyében',
+  //   url: '/event',
+  //   hely: '1358 Bp, Széchenyi rkp. 19.',
+  //   idopont: '10:00 – 12:00',
+  //   kepUrl: 'assets/images/event/202005201000.jpg',
+  //   htmlContent: `<p>Donec porta diam eu massa. Quisque diam lorem, interdum vitae, dapibus ac, scelerisque vitae, pede. Donec eget tellus non erat lacinia fermentum. Donec in velit vel ipsum auctor pulvinar. Vestibulum iaculis lacinia est. Proin dictum elementum velit. Fusce euismod consequat ante. Lorem ipsum dolor sit amet, consectetuer adipis. Mauris accumsan nulla vel diam.</p>`
+  // },
+  // {
+  //   id: 2,
+  //   datum: new Date('2021-08-01T22:00:00'),
+  //   cim: 'Jótékonysági est',
+  //   url: '/event',
+  //   hely: '1095 Bp, Komor Marcell u. 1.',
+  //   idopont: '18:00 – 22:30',
+  //   kepUrl: 'assets/images/event/202005281000.jpg',
+  //   htmlContent: `<p>Donec porta diam eu massa. Quisque diam lorem, interdum vitae, dapibus ac, scelerisque vitae, pede. Donec eget tellus non erat lacinia fermentum. Donec in velit vel ipsum auctor pulvinar. Vestibulum iaculis lacinia est. Proin dictum elementum velit. Fusce euismod consequat ante. Lorem ipsum dolor sit amet, consectetuer adipis. Mauris accumsan nulla vel diam.</p>`
+  // },
+  // {
+  //   id: 3,
+  //   datum: new Date('2021-08-16T22:00:00'),
+  //   cim: 'Rossz Event',
+  //   url: '/event',
+  //   hely: '1095 Bp, Komor Marcell u. 1.',
+  //   idopont: '18:00 – 22:30',
+  //   kepUrl: 'assets/images/event/202005281000.jpg',
+  //   htmlContent: `<p>Donec porta diam eu massa. Quisque diam lorem, interdum vitae, dapibus ac, scelerisque vitae, pede. Donec eget tellus non erat lacinia fermentum. Donec in velit vel ipsum auctor pulvinar. Vestibulum iaculis lacinia est. Proin dictum elementum velit. Fusce euismod consequat ante. Lorem ipsum dolor sit amet, consectetuer adipis. Mauris accumsan nulla vel diam.</p>`
+  // }
+  {
+    id: 1,
+    datum: new Date('2021-08-13T09:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2100 Gödöllő Dózsa György út 1-3. (Hohn Krisztina irodája előtt.)',
+    idopont: '09:00 – 11:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.<br />Helypontosítás: Hohn Krisztina irodája előtt.</p>`
+  },
+  {
+    id: 2,
+    datum: new Date('2021-08-13T13:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2143 Kistarcsa Hunyadi utca 2. (Coop élelmiszerbolt előtt.)',
+    idopont: '13:00 – 15:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.<br />Helypontosítás: Coop élelmiszerbolt előtt.</p>`
+  },
+  {
+    id: 3,
+    datum: new Date('2021-08-14T08:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2170 Aszód Kondoros tér, piac bejárat előtt',
+    idopont: '08:00 – 11:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.</p>`
+  },
+  {
+    id: 4,
+    datum: new Date('2021-08-18T09:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2142 Nagytarcsa Sztehlo Gábor utca -Petőfi utca sarka',
+    idopont: '09:00 – 11:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.<br />Helypontosítás: Partner ABC előtt.</p>`
+  },
+  {
+    id: 5,
+    datum: new Date('2021-08-18T13:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2174 Verseg Postahivatal, Fő utca 3.',
+    idopont: '13:00 – 14:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.</p>`
+  },
+  {
+    id: 6,
+    datum: new Date('2021-08-23T13:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2100 Gödöllő Dózsa György út 1-3. (Hohn Krisztina irodája előtt.)',
+    idopont: '13:00 – 16:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.<br />Helypontosítás: Hohn Krisztina irodája előtt.</p>`
+  },
+  {
+    id: 7,
+    datum: new Date('2021-08-24T08:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2100 Gödöllő Főtér, a piachoz közel',
+    idopont: '08:00 – 11:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.</p>`
+  },
+  {
+    id: 8,
+    datum: new Date('2021-08-24T14:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2117 Isaszeg vasútállomás, Tescoval szemben',
+    idopont: '14:00 – 15:30',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.</p>`
+  },
+  {
+    id: 9,
+    datum: new Date('2021-08-25T08:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2100 Gödöllő Főtér, a piachoz közel',
+    idopont: '08:00 – 11:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.</p>`
+  },
+  {
+    id: 10,
+    datum: new Date('2021-08-25T14:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2143 Kistarcsa Eperjesiút 26. (Sárvári ABC előtt.)',
+    idopont: '14:00 – 16:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.<br />Helypontosítás: Sárvári ABC előtt.</p>`
+  },
+  {
+    id: 11,
+    datum: new Date('2021-08-27T08:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2100 Gödöllő, piac',
+    idopont: '08:00 – 11:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.</p>`
+  },
+  {
+    id: 12,
+    datum: new Date('2021-08-28T08:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2100 Gödöllő, piac',
+    idopont: '08:00 – 11:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.</p>`
+  },
+  {
+    id: 13,
+    datum: new Date('2021-08-28T09:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2170 Aszód Kondoros tér',
+    idopont: '09:00 – 11:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.</p>`
+  },
+  {
+    id: 14,
+    datum: new Date('2021-08-30T09:30:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2100 Gödöllő Dózsa György út 1-3. (Hohn Krisztina irodája előtt.)',
+    idopont: '09:30 – 11:30',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.<br />Helypontosítás: Hohn Krisztina irodája előtt.</p>`
+  },
+  {
+    id: 15,
+    datum: new Date('2021-08-30T14:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2117 Isaszeg vasútállomás, Tescoval szemben',
+    idopont: '14:00 – 15:30',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.</p>`
+  },
+  {
+    id: 16,
+    datum: new Date('2021-08-31T08:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2100 Gödöllő Főtér, a piachoz közel',
+    idopont: '08:00 – 11:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.</p>`
+  },
+  {
+    id: 17,
+    datum: new Date('2021-08-31T14:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2111 Szada, Coop mellett',
+    idopont: '14:00 – 16:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.</p>`
+  },
+  {
+    id: 18,
+    datum: new Date('2021-09-03T09:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2100 Gödöllő, piac',
+    idopont: '09:00 – 11:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.</p>`
+  },
+  {
+    id: 19,
+    datum: new Date('2021-09-03T14:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2117 Isaszeg vasútállomás, Tescoval szemben',
+    idopont: '14:00 – 16:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.</p>`
+  },
+  {
+    id: 20,
+    datum: new Date('2021-09-04T08:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2100 Gödöllő, piac',
+    idopont: '08:00 – 11:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.</p>`
+  },
+  {
+    id: 21,
+    datum: new Date('2021-09-06T09:30:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2173 Kartal Petőfi utca',
+    idopont: '09:30 – 11:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.</p>`
+  },
+  {
+    id: 22,
+    datum: new Date('2021-09-06T14:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2146 Mogyoród Petőfi út',
+    idopont: '14:00 – 15:30',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.</p>`
+  },
+  {
+    id: 23,
+    datum: new Date('2021-09-10T08:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2100 Gödöllő, piac',
+    idopont: '08:00 – 11:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.</p>`
+  },
+  {
+    id: 24,
+    datum: new Date('2021-09-11T08:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2100 Gödöllő, piac',
+    idopont: '08:00 – 11:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.</p>`
+  },
+  {
+    id: 25,
+    datum: new Date('2021-09-11T08:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2170 Aszód Kondoros tér',
+    idopont: '08:00 – 11:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.</p>`
+  },
+  {
+    id: 26,
+    datum: new Date('2021-09-14T09:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2142 Nagytarcsa Sztehlo Gábor utca -Petőfi utca sarka',
+    idopont: '09:00 – 11:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.<br />Helypontosítás: Partner ABC előtt.</p>`
+  },
+  {
+    id: 27,
+    datum: new Date('2021-09-14T13:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2100 Gödöllő Dózsa György út 1-3. (Hohn Krisztina irodája előtt.)',
+    idopont: '13:00 – 16:30',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.<br />Helypontosítás: Hohn Krisztina irodája előtt.</p>`
+  },
+  {
+    id: 28,
+    datum: new Date('2021-09-17T09:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2100 Gödöllő, piac',
+    idopont: '09:00 – 11:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.</p>`
+  },
+  {
+    id: 29,
+    datum: new Date('2021-09-18T09:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2100 Gödöllő, piac',
+    idopont: '09:00 – 11:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.</p>`
+  },
+  {
+    id: 30,
+    datum: new Date('2021-09-20T09:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2100 Gödöllő Dózsa György út 1-3. (Hohn Krisztina irodája előtt.)',
+    idopont: '09:00 – 11:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.<br />Helypontosítás: Hohn Krisztina irodája előtt.</p>`
+  },
+  {
+    id: 31,
+    datum: new Date('2021-09-21T09:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2117 Isaszeg vasútállomás, Tescoval szemben',
+    idopont: '09:00 – 11:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.</p>`
+  },
+  {
+    id: 32,
+    datum: new Date('2021-09-23T09:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2100 Gödöllő, piac',
+    idopont: '09:00 – 11:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.</p>`
+  },
+  {
+    id: 33,
+    datum: new Date('2021-09-24T09:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2100 Gödöllő, piac',
+    idopont: '09:00 – 11:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.</p>`
+  },
+  {
+    id: 34,
+    datum: new Date('2021-09-24T14:00:00'),
+    cim: 'Köztéri fogadóóra és támogató aláírásgyűjtés',
+    url: '/event',
+    hely: '2143 Kistarcsa (Zöld ABC előtt.)',
+    idopont: '14:00 – 16:00',
+    kepUrl: 'assets/images/event/fogadoora.jpg',
+    htmlContent: `<p>Köztéri fogadóóra és támogató aláírásgyűjtés.<br />Helypontosítás: Zöld ABC előtt.</p>`
+  }
+];
